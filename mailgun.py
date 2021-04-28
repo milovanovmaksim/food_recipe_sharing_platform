a5686131fd90e3e68f9f243317e74c523ef2a3fb
maxim <milovanov160386@gmail.com>
2021-04-28 20:54:09 +0300

    удалил не используемый код

--- mailgun.py
+++ mailgun.py
@@ -9,7 +9,6 @@ class MailGunApi:
         self.api_key = api_key
         self.base_url = self.API_URL.format(self.domain)
 
-
     def send_email(self, to, subject, text, html=None):
         if not isinstance(to, (list, tuple)):
             to = [to]
@@ -22,10 +21,3 @@ class MailGunApi:
         }
         response = requests.post(url=self.base_url, auth=('api', self.api_key), data=data)
         return response
-
-
-# mailgun = MailGunApi(domain='sandbox0a7de0526e124502ac155429dd35f50f.mailgun.org',
-#                      api_key='f001a4905fb0ee408a7e4f8d6dd6f700-4b1aa784-66cf0072')
-#
-# mailgun.send_email(to=['milovanov160386@gmail.com'], subject='Hello', text='Тестируем MAilGun')
-
