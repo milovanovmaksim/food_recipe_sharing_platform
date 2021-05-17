9dcf637c7128aece955ccc3da6687657b026ce8b
milovanov <milovanov160386@gmail.com>
2021-05-17 15:32:06 +0300

    добавил ProductionConfig

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-from os import path
+from os import path, environ
 
 base_dir = path.abspath(path.dirname(__file__))
 
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'super-secret-key'
     JWT_ERROR_MESSAGE_KEY = 'message'
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
@@ -15,15 +14,20 @@ class Config:
     RATELIMIT_HEADERS_ENABLED = True
 
 
-
-
 class DevelopmentConfig(Config):
     DEBUG = True
+    SECRET_KEY = 'super-secret-key'
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://maxim:canada@localhost:5434/smilecook'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(base_dir, 'data_dev.sqlite')
 
 
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+    SECRET_KEY = environ.get('SECRET_KEY')
+
+
 config = {
     'development': DevelopmentConfig,
-    'default': DevelopmentConfig
+    'default': DevelopmentConfig,
+    'production': ProductionConfig
 }
